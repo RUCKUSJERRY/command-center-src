7c437cf5eef63dc2cba636b516214bfe93a68bb8
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2023-02-11 16:55:11 +0900

    로그인 예시 파일 추가

---
+++ command-center-front/src/page/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await response.json();
+      if (data.success) {
+        console.log("Login Successful");
+      } else {
+        console.log("Login Failed");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+     <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Login;
