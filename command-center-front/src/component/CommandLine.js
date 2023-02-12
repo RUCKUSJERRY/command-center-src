79e3de125473c126f7521cf647b6bb2ca40642e2
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2023-02-12 15:33:15 +0900

    alt와 tab 입력시 focusControl 하도록함

--- command-center-front/src/component/CommandLine.js
+++ command-center-front/src/component/CommandLine.js
@@ -2,13 +2,16 @@ import React  from 'react';
 
 const CommandLine = () => {
 
+    // keydown시 focusControl을 한다.
+    document.addEventListener("keydown", focusControl);
+
     var keys = [];
 
     function keysEvent(e) {    
       const commandview = document.getElementById('commandview'); // 커멘드를 보여주는 곳
       const commandline = document.getElementById('commandline'); // 커멘드를 입력하는 곳
-      console.log(commandline.value);
 
+      // 한글은 입력되지 않도록 한다. command를 사용할때 필요없는 정보임
       var expression = RegExp(/^[ㄱ-ㅎ|가-힣]+$/);
       if (expression.test(commandline.value)){
         alert("please enter English");  
@@ -24,13 +27,34 @@ const CommandLine = () => {
           }
         };
 
+        var viewText = "";
+        for (var i in keys) {
+          console.log(i);
+          if (i > 0) {
+            viewText += " + " + keys[i];
+          } else {
+            viewText += keys[i];
+          } 
+
+        }
+
         // 현재 입력한 커멘드 값을 화면에 보여준다.
-        commandview.innerHTML = keys;
+        commandview.innerHTML = viewText;
       }
 
       // commandline의 value를 초기화 해준다.
       commandline.value = null;
 
+      //focusControl(e.keyCode);
+
+    }
+
+    function focusControl(e) { 
+      // alt or tab을 입력한 경우에는 focus를 다시 input으로 옮겨준다.
+      if (e.keyCode === 18 || e.keyCode === 9) {
+        document.getElementById('commandline').focus(); // 커멘드를 입력하는 곳
+        e.preventDefault();
+      }
     }
 
     return (
