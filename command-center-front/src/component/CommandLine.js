9af8265284eb58fede4a77effc493e2fd886bc54
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2023-02-12 14:13:46 +0900

    여러 키 동시 이벤트 제어 진행중

--- command-center-front/src/component/CommandLine.js
+++ command-center-front/src/component/CommandLine.js
@@ -2,6 +2,29 @@ import React, { useState }  from 'react';
 
 const CommandLine = () => {
 
+  window.addEventListener("keydown", keysPressed, false);
+  window.addEventListener("keyup", keysReleased, false);
+  
+  var keys = [];
+  
+    function keysPressed(e) {
+      // store an entry for every key pressed
+      keys[e.keyCode] = true;
+      console.log('keys = ' + e.keyCode);
+      // Ctrl + Shift + 5
+      if (keys[17] && keys[16] && keys[53]) {
+        // do something
+      }
+      
+      // Ctrl + f
+      if (keys[17] && keys[70]) {
+        // do something
+      
+        // prevent default browser behavior
+        e.preventDefault();	
+      }
+    }
+
     const [commandData] = useState({
         command: "",
       });
@@ -13,6 +36,11 @@ const CommandLine = () => {
       }
       //commandData = event.key;
     };
+    
+    function keysReleased(e) {
+      // mark keys that were released
+      keys[e.keyCode] = false;
+    }
 
     return (
         <input
